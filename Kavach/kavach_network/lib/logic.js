6440f9ecfe69a12b96d44113e5fe406fd1b00fc0
murvi08 <urvimandge@gmail.com>
2019-03-16 13:16:32 +0530

    transaction commit

--- Kavach/kavach_network/lib/logic.js
+++ Kavach/kavach_network/lib/logic.js
@@ -12,17 +12,37 @@
  * limitations under the License.
  */
 
-'use strict';
+//'use strict';
 /**
  * Write your transction processor functions here
  */
 
 /**
  * Sample transaction
- * @param {kavachnetwork.SampleTransaction} sampleTransaction
+ * @param {kavachnetwork.PutDtBlock} dataReq
  * @transaction
  */
-async function sampleTransaction(tx) {
+/*async function sampleTransaction() {
+    // Save the old value of the asset.
+    const oldValue = tx.asset.value;
+
+    // Update the asset with the new value.
+    tx.asset.value = tx.newValue;
+
+    // Get the asset registry for the asset.
+    const assetRegistry = await getAssetRegistry('kavachnetwork.SampleAsset');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.asset);
+
+    // Emit an event for the modified asset.
+    let event = getFactory().newEvent('kavachnetwork', 'SampleEvent');
+    event.asset = tx.asset;
+    event.oldValue = oldValue;
+    event.newValue = tx.newValue;
+    emit(event);
+}*/
+
+function putDtBlock(dataReq) {
     // Save the old value of the asset.
     const oldValue = tx.asset.value;
 
